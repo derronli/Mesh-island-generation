ff78373a94fcfa68475576a141eed42cc64d9d4b
Kyle Hagerman <khagypi3@gmail.com>
2023-02-08 13:42:43 -0500

    Renamed vertex and segment classes

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MySegment.java
@@ -0,0 +1,6 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
+
+public class MySegment {
+}
---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MyVertex.java
@@ -0,0 +1,10 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+
+public class MyVertex {
+
+    private int index;
+
+
+}
