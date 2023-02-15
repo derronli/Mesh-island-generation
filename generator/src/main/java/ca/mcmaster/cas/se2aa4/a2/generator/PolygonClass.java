d1a06680c83f36f3db1eae40fc49046a40dbf35e
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-02-15 10:57:01 -0500

    Create PolygonClass.java

---
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/PolygonClass.java
@@ -0,0 +1,22 @@
+package ca.mcmaster.cas.se2aa4.a2.generator;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+
+import java.util.List;
+
+public class PolygonClass {
+    //pass in segments
+    //order the segments based on if they are adjacent
+    //calculate the centroid of a polygon based on the segments
+
+    private int width = 500;
+    private int height = 500;
+    private int square_size = 20;
+
+    private List <MySegment> segments;
+    private MyVertex centroid;
+
+
+
+}
