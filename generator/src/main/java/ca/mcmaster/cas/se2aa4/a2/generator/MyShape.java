5a7e2013761bb850dcfa42c9324ce144c6bc57f7
Kyle Hagerman <khagypi3@gmail.com>
2023-02-24 14:16:51 -0500

    Commented out methods not used in interface

--- generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MyShape.java
+++ generator/src/main/java/ca/mcmaster/cas/se2aa4/a2/generator/MyShape.java
@@ -1,12 +1,12 @@
 package ca.mcmaster.cas.se2aa4.a2.generator;
 
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
+//import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 
 public interface MyShape {
 
     void setTrans(int alpha);
     void setThick(float thickness);
-    int getIndex();
-    int[] getColor();
-    java.util.List<Property> getPropertiesList();
+//    int getIndex();
+//    int[] getColor();
+//    java.util.List<Property> getPropertiesList();
 }
