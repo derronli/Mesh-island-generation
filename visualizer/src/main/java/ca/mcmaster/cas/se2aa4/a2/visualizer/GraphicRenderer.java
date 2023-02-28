6a49109b89f341707fd685753644ebf7ec744881
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-02-27 22:56:20 -0500

    Cleaning debug and graphic renderer

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -1,21 +1,11 @@
 package ca.mcmaster.cas.se2aa4.a2.visualizer;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Property;
 
 import java.awt.Graphics2D;
-import java.awt.Stroke;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Line2D;
-import java.util.List;
 
 public class GraphicRenderer {
-
-    private static final int THICKNESS = 3;
+    
     public void render(Mesh aMesh, Graphics2D canvas, String commandArg) {
 
         MyRenderer renderer;
