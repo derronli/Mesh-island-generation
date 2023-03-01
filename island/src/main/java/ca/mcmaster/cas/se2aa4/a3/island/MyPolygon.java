89b82ecad297194fe8c37969b4de64eefab60eb3
Kyle Hagerman <khagypi3@gmail.com>
2023-03-01 14:44:33 -0500

    Added indices to shapes

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/MyPolygon.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/MyPolygon.java
@@ -5,17 +5,30 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs.Polygon;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
+import java.util.List;
+
 public class MyPolygon implements MyShape{
 
+    private static int totalIndex = 0;
+    private final int index;
     private Polygon polygon;
     private TileType myTile;
 
     public MyPolygon(Polygon p){
         polygon = p;
+        index = totalIndex;
+        totalIndex++;
+    }
+
+    private void initPolygon(){
+
     }
 
     public void changeColor(String colorCode){
 //        Structs.Property color = Structs.Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
 //        polygon = Polygon.newBuilder(polygon).setProperties(0, color).build();
     }
+
+    // Getters
+    public int getIndex(){ return index; }
 }
--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/MySegment.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/MySegment.java
@@ -6,14 +6,21 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs.Segment;
 
 public class MySegment implements MyShape{
 
+    private static int totalIndex = 0;
+    private final int index;
     private Segment segment;
 
     public MySegment(Segment s){
         segment = s;
+        index = totalIndex;
+        totalIndex++;
     }
 
     public void changeColor(String colorCode){
         Structs.Property color = Structs.Property.newBuilder().setKey("rgb_color").setValue(colorCode).build();
         segment = Segment.newBuilder(segment).setProperties(0, color).build();
     }
+
+    // Getters
+    public int getIndex(){ return index; }
 }
