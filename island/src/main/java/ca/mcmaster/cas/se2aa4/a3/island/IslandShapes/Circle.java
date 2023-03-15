415f0087a0bfa2eb968200cef6dc4264f91f390e
Kyle Hagerman <khagypi3@gmail.com>
2023-03-15 09:20:44 -0400

    Circle shape update

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/IslandShapes/Circle.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/IslandShapes/Circle.java
@@ -2,12 +2,16 @@ package ca.mcmaster.cas.se2aa4.a3.island.IslandShapes;
 
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.util.GeometricShapeFactory;
 
 public class Circle implements IslandShape{
 
     @Override
     public Geometry getShape(int width, int height) {
         Coordinate midpoint = new Coordinate(width / 2.0, height / 2.0);
+        GeometricShapeFactory gsf = new GeometricShapeFactory();
+       // gsf.setSize((width + height) / 2.0 - );
+
         return null;
     }
 
