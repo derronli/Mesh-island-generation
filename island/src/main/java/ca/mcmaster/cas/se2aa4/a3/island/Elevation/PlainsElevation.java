d1a64dc9be382ff47996be48144de07fe13db6e9
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-03-21 02:10:42 -0400

    Continued volcanoElevation

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Elevation/PlainsElevation.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Elevation/PlainsElevation.java
@@ -12,7 +12,7 @@ public class PlainsElevation implements BaseElevation {
     }
 
     @Override
-    public void selectElevation() {
+    public void setElevation() {
 
     }
 
--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Elevation/VolcanoElevation.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Elevation/VolcanoElevation.java
@@ -1,6 +1,22 @@
 package ca.mcmaster.cas.se2aa4.a3.island.Elevation;
 
+import ca.mcmaster.cas.se2aa4.a3.island.MyPolygon;
+import ca.mcmaster.cas.se2aa4.a3.island.MySegment;
+import ca.mcmaster.cas.se2aa4.a3.island.MyVertex;
+import java.util.Comparator;
+import java.awt.geom.Point2D;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//need getters and setters of elevation in the polygon and segments classes
+//need to modify vertex constructor
 public class VolcanoElevation implements BaseElevation {
+
+    private List <MyPolygon> polygons;
+    private List <MyVertex> vertices;
+    private List <MySegment> segments;
+    private List <Boolean> markedSegments;
 
     @Override
     public void generateElevation() {
@@ -8,8 +24,35 @@ public class VolcanoElevation implements BaseElevation {
     }
 
     @Override
-    public void selectElevation() {
+    public void setElevation() {
 
+    }
+    private double[] getMiddle(MySegment segment){
+        return new double[]{(segment.getV1X() + segment.getV2X()) / 2, (segment.getV1Y() + segment.getV2Y()) / 2};
+    }
+    private MyVertex calcCentroid(){
+        double midX = 0;
+        double midY = 0;
+        double [] middle;
+        for (MySegment segment : segments) {
+            middle = getMiddle(segment);
+            midX += middle[0];
+            midY += middle[1];
+        }
+        double x = midX/segments.size();
+        double y = midY/segments.size();
+
+        return null; //NEED TO SET THE VERTEX ASK KYLE LATER
+
+    }
+
+    private List<MyVertex> findAllCentroids () {
+        List<MyVertex> allCentroids = new ArrayList<>();
+        for (int i = 0; i<polygons.size(); i++){
+            MyVertex cent = calcCentroid();
+            allCentroids.add(cent);
+        }
+        return allCentroids;
     }
 
     private void findCentrePolygon (){
