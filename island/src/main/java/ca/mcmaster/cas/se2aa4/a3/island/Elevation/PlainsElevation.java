117bb0cf75742ad0b34e9309bdca9dede7fef4c5
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-03-21 00:00:11 -0400

    Created VolcanoElevation class - F03

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Elevation/PlainsElevation.java
@@ -0,0 +1,13 @@
+package ca.mcmaster.cas.se2aa4.a3.island.Elevation;
+//plains can go here
+public class PlainsElevation implements BaseElevation {
+    @Override
+    public void generateElevation() {
+
+    }
+
+    @Override
+    public void selectElevation() {
+
+    }
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Elevation/VolcanoElevation.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.cas.se2aa4.a3.island.Elevation;
+
+public class VolcanoElevation implements BaseElevation {
+
+    @Override
+    public void generateElevation() {
+
+    }
+
+    @Override
+    public void selectElevation() {
+
+    }
+}
