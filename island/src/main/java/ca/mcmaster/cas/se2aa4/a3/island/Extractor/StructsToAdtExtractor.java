66a788330e0bd25cc80b2e91e1b40bde7bbcf9cc
Derron Li <104483680+derronli@users.noreply.github.com>
2023-03-20 23:16:56 -0400

    Added an Extractor Class

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Extractor/StructsToAdtExtractor.java
@@ -0,0 +1,69 @@
+package ca.mcmaster.cas.se2aa4.a3.island.Extractor;
+
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+import ca.mcmaster.cas.se2aa4.a3.island.MyPolygon;
+import ca.mcmaster.cas.se2aa4.a3.island.MySegment;
+import ca.mcmaster.cas.se2aa4.a3.island.MyVertex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StructsToAdtExtractor {
+
+    private final List<MyVertex> myVertices = new ArrayList<>();
+    private final List<MySegment> mySegments = new ArrayList<>();
+    private final List<MyPolygon> myPolygons = new ArrayList<>();
+
+    public StructsToAdtExtractor(Structs.Mesh aMesh) {
+        List<Structs.Vertex> vertices = aMesh.getVerticesList();
+        List<Structs.Segment> segments = aMesh.getSegmentsList();
+        List<Structs.Polygon> polygons = aMesh.getPolygonsList();
+
+        // Add all items to my structure.
+        for (Structs.Vertex v : vertices){
+            MyVertex myV = new MyVertex(v);
+            myV.changeColor("0,0,0");
+            myVertices.add(myV);
+        }
+        for (Structs.Segment s : segments){
+            MySegment myS = new MySegment(s);
+            myS.changeColor("0,0,0");
+            mySegments.add(myS);
+        }
+        for (Structs.Polygon p : polygons){
+            MyPolygon myP = new MyPolygon(p);
+            myP.changeColor("255,255,255");
+            myPolygons.add(myP);
+        }
+
+        addVerticesToSegments(myVertices, mySegments);
+        addSegmentsToPolygons(mySegments, myPolygons);
+
+    }
+
+    // Adds MyVertex instances to MySegment as a field.
+    private void addVerticesToSegments(List<MyVertex> myVertices, List<MySegment> mySegments){
+        for (MySegment s : mySegments){
+            int v1Idx = s.getV1Index();
+            int v2Idx = s.getV2Index();
+            MyVertex v1 = myVertices.get(v1Idx);
+            MyVertex v2 = myVertices.get(v2Idx);
+            s.setV1(v1); s.setV2(v2);
+        }
+    }
+
+    // Add MySegment instances to MyPolygon as a field.
+    private void addSegmentsToPolygons(List<MySegment> mySegments, List<MyPolygon> myPolygons){
+        for (MyPolygon p : myPolygons){
+            for (Integer idx : p.getSegmentIdxsList()){
+                MySegment s = mySegments.get(idx);
+                p.addSegment(s);
+            }
+        }
+    }
+
+    public List<MyPolygon> getMyPolygons() {return myPolygons;}
+    public List<MySegment> getMySegments() {return mySegments;}
+    public List<MyVertex> getMyVertices() {return myVertices;}
+
+}
---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/FreshWater/LakeGenerator.java
@@ -0,0 +1,9 @@
+package ca.mcmaster.cas.se2aa4.a3.island.FreshWater;
+
+public class LakeGenerator {
+    // Args specify loop bounds
+    // while (args)
+    // Get Random Number within bounds of the myPolygons array list length (access random index)
+    // Check if the polygon is valid (is it land?) -> set to lake "type" + save this polygon into a lake's array?
+    // decrement args counter if it is valid, otherwise continue
+}
