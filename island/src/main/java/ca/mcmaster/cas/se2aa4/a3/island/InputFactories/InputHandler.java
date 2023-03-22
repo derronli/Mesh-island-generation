7c28682628911265f34128c6b577523bda7fd7a2
Kyle Hagerman <khagypi3@gmail.com>
2023-03-22 12:31:29 -0400

    Added builder factory for user input when choosing shape

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/InputFactories/InputHandler.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/InputFactories/InputHandler.java
@@ -1,4 +1,4 @@
-package ca.mcmaster.cas.se2aa4.a3.island;
+package ca.mcmaster.cas.se2aa4.a3.island.InputFactories;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 import ca.mcmaster.cas.se2aa4.a3.island.Builders.*;
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InputHandler {
-
-    private final Map<String, IslandShape> builderOptions = createBuilderOptions();
     private final Map<String, HeatmapPainter> heatmapOptions = createHeatmapOptions();
 
 
@@ -20,15 +18,7 @@ public class InputHandler {
     public InputHandler(String mode){
         this.mode = mode;
     }
-    
-    private Map<String, IslandShape> createBuilderOptions(){
-        int width = 500; int height = 500;
-        Map<String, IslandShape> options = new HashMap<>();
-        options.put("circle", new Circle(width, height));
-        options.put("hexagon", new Hexagon(width, height));
 
-        return options;
-    }
     private Map<String, HeatmapPainter> createHeatmapOptions(){
         Map<String, HeatmapPainter> options = new HashMap<>();
         options.put("elevation", new ElevationPainter());
@@ -50,14 +40,10 @@ public class InputHandler {
             d = new LagoonBuilder();
         }
         else {
-
             // Makes a mesh of the specified type if not lagoon mode.
-            IslandShape shape = builderOptions.get(mode);
-            if (shape == null) {
-                throw new IllegalArgumentException("mode must be of a valid type. Check documentation for more information.");
-            }
+            BuilderFactory builderFactory = new BuilderFactory();
+            IslandShape shape = builderFactory.getIslandShape(mode);
             d = new IslandBuilder(shape);
-
         }
 
         d.buildIsland(aMesh);
