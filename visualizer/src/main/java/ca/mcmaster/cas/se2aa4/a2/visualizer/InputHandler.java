e4f09a548df0f740714baada7aa443235d3adbbb
Kyle Hagerman <khagypi3@gmail.com>
2023-03-22 10:52:48 -0400

    Temporary heatmap mode in visualizer to debug eleavtion

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/InputHandler.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/InputHandler.java
@@ -1,122 +1,82 @@
 package ca.mcmaster.cas.se2aa4.a2.visualizer;
 
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-import org.apache.commons.cli.*;
+import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InputHandler {
 
-    private final float VERTEXTHICKNESS = 3f;
-    private final float POLYSEGTHICKNESS = 0.5f;
-    private final int alpha = 255;
-    private final int numPolygons = 100;
-    private final int relaxation = 0;
+    private final Map<String, MapDebugRenderer> heatmapOptions = createHeatmapOptions();
 
-    public void visualizeMesh(String[] args){
-
-        Options options = createOptions();
-
-        // Parses options with arguments.
-        CommandLineParser parser = new DefaultParser();
-        try {
-            // parse the command line arguments
-            CommandLine line = parser.parse(options, args);
-            checkOptions(line, options);
-        }
-        catch (ParseException exp) {
-            // oops, something went wrong
-            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
-        }
-
-    }
-
-    private Options createOptions(){
-        Options options = new Options();
-
-        Option help = new Option("h", "display all possible inputs");
-        Option type = new Option("ir", "creates an irregular mesh if used, grid if not used");
-        Option pa = Option.builder("pa")
-                .argName("transparency")
-                .hasArg()
-                .desc("define polygon transparency (value from 0 to 255) (default = 255)")
-                .build();
-        Option sa = Option.builder("sa")
-                .argName("transparency")
-                .hasArg()
-                .desc("define segment transparency (value from 0 to 255) (default = 255)")
-                .build();
-        Option va = Option.builder("va")
-                .argName("transparency")
-                .hasArg()
-                .desc("define vertex transparency (value from 0 to 255) (default = 255)")
-                .build();
-        Option pt = Option.builder("pt")
-                .argName("thickness")
-                .hasArg()
-                .desc("define polygon thickness (value from 0 to 255) (default = 255)")
-                .build();
-        Option st = Option.builder("st")
-                .argName("thickness")
-                .hasArg()
-                .desc("define segment thickness (value from 0 to 255) (default = 255)")
-                .build();
-        Option vt = Option.builder("vt")
-                .argName("thickness")
-                .hasArg()
-                .desc("define vertex thickness (value from 0 to 255) (default = 255)")
-                .build();
-        Option np = Option.builder("np")
-                .argName("number of polygons")
-                .hasArg()
-                .desc("Choose number of polygons in irregular mesh (default = 30)")
-                .build();
-        Option rl = Option.builder("rl")
-                .argName("relaxation level")
-                .hasArg()
-                .desc("Choose number of lloyd relaxations to apply in irregular mesh (default = 0)")
-                .build();
-
-        // add all options
-        options.addOption(help);
-        options.addOption(type);
-        options.addOption(pa);
-        options.addOption(sa);
-        options.addOption(va);
-        options.addOption(pt);
-        options.addOption(st);
-        options.addOption(vt);
-        options.addOption(np);
-        options.addOption(rl);
+    private Map<String, MapDebugRenderer> createHeatmapOptions(){
+        Map<String, MapDebugRenderer> options = new HashMap<>();
+        options.put("elevation", new ElevationRenderer());
+        options.put("moisture", new MoistureRenderer());
 
         return options;
     }
 
-    private void displayHelp(Options options){
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("generator help", options);
+    // Visualize mesh in debug mode or regular.
+    public void visualizeMesh(String input, String output, String debug) throws IOException {
+
+        // Getting width and height for the canvas
+        Structs.Mesh aMesh = new MeshFactory().read(input);
+
+        // Creating the Canvas to draw the mesh
+        Graphics2D canvas = canvasFromMesh(aMesh);
+
+        MyRenderer renderer;
+        if (debug != null) {
+            renderer = new DebugRenderer();
+        }
+        else{
+            renderer = new NormalRender();
+        }
+        renderer.render(aMesh, canvas);
+
+        meshToFile(output, canvas, aMesh);
+
     }
 
-    private void checkOptions(CommandLine line, Options options){
+    // Visualize mesh in heatmap mode.
+    public void visualizeHeatmap(String input, String output, String heatmap) throws IOException {
 
-        String inputFile = null, outputFile = null, mode = null;
+        // Getting width and height for the canvas
+        Structs.Mesh aMesh = new MeshFactory().read(input);
 
-        // If they ask for help, displays options, and exits without generating a mesh.
-        if (line.hasOption("h")){
-            displayHelp(options);
-            return;
+        // Creating the Canvas to draw the mesh
+        Graphics2D canvas = canvasFromMesh(aMesh);
+
+        // Painting the mesh on the canvas
+        MapDebugRenderer renderer = heatmapOptions.get(heatmap);
+        renderer.render(aMesh, canvas);
+
+        meshToFile(output, canvas, aMesh);
+
+    }
+
+    private Graphics2D canvasFromMesh(Structs.Mesh aMesh){
+        double max_x = Double.MIN_VALUE;
+        double max_y = Double.MIN_VALUE;
+        for (Structs.Vertex v: aMesh.getVerticesList()) {
+            max_x = (Double.compare(max_x, v.getX()) < 0? v.getX(): max_x);
+            max_y = (Double.compare(max_y, v.getY()) < 0? v.getY(): max_y);
         }
-        if (line.hasOption("i")){
-            inputFile = line.getOptionValue("i");
-        }
-        if (line.hasOption("o")){
-            outputFile = line.getOptionValue("o");
-        }
-        if (line.hasOption("mode")){
-            mode = line.getOptionValue("mode");
-        }
 
-        return;
+        return SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));
 
+    }
 
+    private void meshToFile(String output, Graphics2D canvas, Structs.Mesh aMesh) throws IOException {
+        // Storing the result in an SVG file
+        SVGCanvas.write(canvas, output);
+        // Dump the mesh to stdout
+        MeshDump dumper = new MeshDump();
+        dumper.dump(aMesh);
     }
 
 }
---
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/InputParser.java
@@ -0,0 +1,105 @@
+package ca.mcmaster.cas.se2aa4.a2.visualizer;
+
+import org.apache.commons.cli.*;
+
+import java.io.IOException;
+
+public class InputParser {
+
+    public void readInput(String[] args){
+
+        Options options = createOptions();
+
+        // Parses options with arguments.
+        CommandLineParser parser = new DefaultParser();
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(options, args);
+            checkOptions(line, options);
+        }
+        catch (ParseException | IOException exp) {
+            // oops, something went wrong
+            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+        }
+
+
+
+    }
+
+    private Options createOptions(){
+        Options options = new Options();
+
+        Option help = new Option("h", "display all possible inputs");
+        Option debug = new Option("X", "Enter debug mode (A2)");
+        Option heatmap = Option.builder("heatmap")
+                .argName("field")
+                .hasArg()
+                .desc("Choose which field a heatmap is produced from on island")
+                .build();
+        Option input = Option.builder("i")
+                .argName("input file")
+                .hasArg()
+                .desc("file to read mesh from")
+                .build();
+        Option output = Option.builder("o")
+                .argName("output file")
+                .hasArg()
+                .desc("file to output mesh to")
+                .build();
+
+        // add all options
+        options.addOption(help);
+        options.addOption(debug);
+        options.addOption(input);
+        options.addOption(output);
+        options.addOption(heatmap);
+
+        return options;
+    }
+
+    private void displayHelp(Options options){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("visualizer help", options);
+    }
+
+    private void checkOptions(CommandLine line, Options options) throws IOException {
+
+        String inputFile = null, outputFile = null, heatmap = null, debug = null;
+
+        // If they ask for help, displays options, and exits without generating a mesh.
+        if (line.hasOption("h")){
+            displayHelp(options);
+            return;
+        }
+        if (line.hasOption("i")){
+            inputFile = line.getOptionValue("i");
+        }
+        if (line.hasOption("o")){
+            outputFile = line.getOptionValue("o");
+        }
+        if (line.hasOption("debug")){
+            debug = line.getOptionValue("debug");
+        }
+        if (line.hasOption("heatmap")){
+            heatmap = line.getOptionValue("heatmap");
+        }
+
+        InputHandler handler = new InputHandler();
+
+        // Ensures we have an input and output file before visualizing island.
+        if (!(inputFile == null || outputFile == null)){
+            if (heatmap != null && debug != null){
+                throw new IllegalArgumentException("Error, cannot activate mesh debug and heatmap at same time");
+            }
+            if (heatmap != null){
+                handler.visualizeHeatmap(inputFile, outputFile, heatmap);
+            }
+            else{
+                handler.visualizeMesh(inputFile, outputFile, debug);
+            }
+
+        }
+
+    }
+
+}
