06d14447808e6d3d0f3d7236813a55c60aefce36
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-03-24 16:57:36 -0400

    created aquifer classes - F11

---
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/FreshWater/Aquifer.java
@@ -0,0 +1,5 @@
+package ca.mcmaster.cas.se2aa4.a3.island.FreshWater;
+
+public abstract class Aquifer {
+
+}
--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/FreshWater/AquiferGenerator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/FreshWater/AquiferGenerator.java
@@ -9,6 +9,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
+/*
+go through input list of polygons (will input only polygons in island)
+method should take that list as input and nothing else
+randomly choose polygons from list to add aquifers to up to user input number of times (set as field for now)
+will have a method when choosing a polygon to call polygon.hasAquifer or something to ensure aquifer has not been added already and then regen index
+polygon will have a method polygon.addAquifer which takes in no input, and that's all that needs to be called (polygon will handle moisture on its own)
+
+also make when this generator is made, takes in random as parameter in constructor and sets field to it
+*/
+
 public class AquiferGenerator {
 
     private final int numAquifers;
