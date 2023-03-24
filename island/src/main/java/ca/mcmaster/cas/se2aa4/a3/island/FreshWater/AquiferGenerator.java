d0921231d8f1bdd79004b4bbdac85ca467e3a134
Arjun Karthik <77539979+arjunkarthik123@users.noreply.github.com>
2023-03-24 02:07:34 -0400

    Created basic logic to generate aquifers - F11

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/FreshWater/AquiferGenerator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/FreshWater/AquiferGenerator.java
@@ -4,6 +4,7 @@ import ca.mcmaster.cas.se2aa4.a3.island.IslandADTTypes.Tiles.LakeTile;
 import ca.mcmaster.cas.se2aa4.a3.island.MyPolygon;
 import ca.mcmaster.cas.se2aa4.a3.island.IslandADTTypes.Tiles.LakeTile;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -11,21 +12,43 @@ public class AquiferGenerator {
 
     private final int numAquifers;
     private final Random rand = new Random();
-
-    public AquiferGenerator(List<MyPolygon> myPolygons, int numLakes) {
-        this.numAquifers = numLakes;
-        generate(myPolygons);
+    private List <Integer> aquiferValues;
+    public AquiferGenerator(List<MyPolygon> myPolygons, int numAquifers) {
+        //Collections.fill(); fill all tile moistures with 0 initially to be reassigned accordingly 
+        this.numAquifers = numAquifers;
+        createAquifers(myPolygons);
+        //generateRandomAquifers(myPolygons);
     }
-    private void generate(List<MyPolygon> myPolygons) {
-        int count = 0;
-        int index;
-        MyPolygon p;
-
-        while (count < numAquifers) {
-            index = rand.nextInt(myPolygons.size());
-            p = myPolygons.get(index);
-            count += (p.attemptChange(new LakeTile())) ? 1 : 0;
+    private void generateRandomAquifers(List<MyPolygon> polygons) {
+        for (int i = 0; i < numAquifers; i++) {
+            int moisture = rand.nextInt(1, 40);
+            aquiferValues.add(moisture);
         }
     }
 
+    private void assignToRandomTiles (List <MyPolygon> polygons){
+    int counter = 0;
+        for (int j = 0; j<polygons.size(); j++){
+            int assignToPolygon = rand.nextInt(0,2);
+            if (assignToPolygon== 0){
+                //set to tile
+                counter++;
+                for (int k = 0; k<polygons.size(); k++){
+                    if (polygons.get(j).checkForNeighbour(polygons.get(k))){
+                        //set aquiferValue here
+                    }
+                }
+            }
+            if (counter == numAquifers){
+                break;
+            }
+        }
+    }
+
+    private void createAquifers(List<MyPolygon> polygons){
+        generateRandomAquifers(polygons);
+        assignToRandomTiles(polygons);
+    }
 }
+
+
