6f795479331924103a7de7aec41c895d1c468241
Derron Li <104483680+derronli@users.noreply.github.com>
2023-04-06 19:26:25 -0400

    Finished Graph Class to have minimal adjacency list representation of nodes and edges
    some testing in Main

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/GraphADT/Graph.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/GraphADT/Graph.java
@@ -1,19 +1,52 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.GraphADT;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
-    private Map<Node, List<Node>> adjList;
+    private Map<Node, List<Node>> adjList = new HashMap<>();
 
     public void addNode(int id) {
-
+        adjList.putIfAbsent(new Node(id), new ArrayList<>());
     }
 
-    public void removeNode() {}
+    public void removeNode(int id) {
+        // Make a temporary node for sake of looking it up in the hashmap
+        Node tempLookup = new Node(id);
+        adjList.remove(tempLookup);
+    }
 
-    public void addEdge() {}
+    // Input must be 2 preexisting nodes in the graph
+    public void addEdge(int idn, int idm) {
+        Node n = new Node(idn);
+        Node m = new Node(idm);
 
-    public void removeEdge() {}
+        // Ensure that edge doesn't already exist
+        for (Node neighbour : adjList.get(n)) {
+            if (neighbour.equals(m)) {
+                return;
+            }
+        }
+        // Undirected graph
+        adjList.get(n).add(m);
+        adjList.get(m).add(n);
+    }
 
+    public void removeEdge(int idn, int idm) {
+        Node n = new Node(idn);
+        Node m = new Node(idm);
+
+        adjList.get(n).remove(m);
+        adjList.get(m).remove(n);
+    }
+
+    public void printGraph() {
+        Set<Node> nodes = adjList.keySet();
+        for (Node n : nodes) {
+            System.out.print("Node: " + n.getId() + "\tNeighbours:");
+            for (Node adj : adjList.get(n)) {
+                System.out.print(" " + adj.getId());
+            }
+            System.out.println("");
+        }
+    }
 }
--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Main.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/Main.java
@@ -1,5 +1,6 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder;
 
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.GraphADT.Graph;
 import ca.mcmaster.cas.se2aa4.a4.pathfinder.GraphADT.Node;
 
 import java.util.HashMap;
@@ -8,5 +9,23 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
+        Graph g = new Graph();
+
+        g.addNode(0);
+        g.addNode(1);
+        g.addNode(2);
+        g.addNode(3);
+        g.addNode(0);
+
+        g.addEdge(0, 1);
+        g.addEdge(1, 3);
+        g.addEdge(0, 2);
+        g.addEdge(3, 2);
+
+        g.addEdge(0, 3);
+        g.removeEdge(3, 0);
+
+        g.printGraph();
+
     }
 }
