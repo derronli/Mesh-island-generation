cd85f3da731ec25b87f2a82108be4d24ff452135
Derron Li <104483680+derronli@users.noreply.github.com>
2023-04-10 11:06:32 -0400

    Bug fixed CityGenerator to ensure cities are not created on water

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Urbanism/CityGenerator.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/Urbanism/CityGenerator.java
@@ -1,19 +1,26 @@
 package ca.mcmaster.cas.se2aa4.a3.island.Urbanism;
 
 import ca.mcmaster.cas.se2aa4.a3.island.ShapeAdts.MyPolygon;
+import ca.mcmaster.cas.se2aa4.a3.island.ShapeAdts.MySegment;
 import ca.mcmaster.cas.se2aa4.a3.island.ShapeAdts.MyVertex;
 import org.locationtech.jts.geom.Point;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.List;
 
 public class CityGenerator {
 
     private MyVertex pickRandomCentroid(List<MyPolygon> myPolygons, List<MyVertex> myVertices, Random rand) {
-        int pIdx = rand.nextInt(myPolygons.size());
-        MyPolygon p = myPolygons.get(pIdx);
-
-        Point center = p.getCenterOfPolygon();
+        MyPolygon p;
+        Point center;
+        // while loop is for ensuring that the city is not created on a "water tile" (aka lake)
+        // While the pick is a lake -> choose something else
+        do {
+            int pIdx = rand.nextInt(myPolygons.size());
+            p = myPolygons.get(pIdx);
+            center = p.getCenterOfPolygon();
+        } while (p.isWaterTile());
 
         return findVertex(myVertices, center);
     }
@@ -27,12 +34,15 @@ public class CityGenerator {
         return null;
     }
 
-    public void generate(List<MyPolygon> myPolygons, List<MyVertex> myVertices, int numCities, Random rand) {
+    public List<MyVertex> generate(List<MyPolygon> myPolygons, List<MyVertex> myVertices, int numCities, Random rand) {
+        List<MyVertex> citiesList = new ArrayList<>();
         while (numCities != 0) {
             MyVertex centroid = pickRandomCentroid(myPolygons, myVertices, rand);
             if (centroid.makeCityVertex(rand.nextInt(9) + 3)) {
                 numCities--;
+                citiesList.add(centroid);
             }
         }
+        return citiesList;
     }
 }
