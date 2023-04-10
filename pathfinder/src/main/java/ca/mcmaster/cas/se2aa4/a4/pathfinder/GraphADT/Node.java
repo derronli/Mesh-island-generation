c8453eff0874d705c9146daed9c94ae245cdabb3
Derron Li <104483680+derronli@users.noreply.github.com>
2023-04-10 11:05:22 -0400

    Allow nodes to hold attributes by using a hashmap

--- pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/GraphADT/Node.java
+++ pathfinder/src/main/java/ca/mcmaster/cas/se2aa4/a4/pathfinder/GraphADT/Node.java
@@ -1,15 +1,25 @@
 package ca.mcmaster.cas.se2aa4.a4.pathfinder.GraphADT;
 
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.Map;
 
 public class Node {
     private final int id;
+    private Map<String, String> properties = new HashMap<>();
 
     public Node(int id) {
         this.id = id;
     }
 
     public int getId() { return id; }
+
+    public void setProperty(String property, String value) {
+        properties.put(property, value);
+    }
+    public String getProperty(String property) {
+        return properties.get(property);
+    }
 
     // Override equals so it will be defined by if the ids are equivalent
     @Override
